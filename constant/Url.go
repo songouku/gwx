53b9b136cb8a862b407abde791124d43b9a42283
wbx <912005971@qq.com>
2019-10-28 15:35:59 +0800

    接口地址定义

--- constant/Url.go
+++ constant/Url.go
@@ -1,5 +1,34 @@
 package constant
 
 var (
+	//获取token
 	Token = "https://api.weixin.qq.com/cgi-bin/token"
+
+	//创建菜单 POST
+	// https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN
+	CreateMenu = "https://api.weixin.qq.com/cgi-bin/menu/create"
+
+	//获取菜单 POST
+	//https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=ACCESS_TOKEN
+	QueryMenu = "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info"
+
+	//删除菜单 GET
+	//https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN
+	DelMenu = "https://api.weixin.qq.com/cgi-bin/menu/delete"
+
+	//创建个性化菜单 POST
+	//https://api.weixin.qq.com/cgi-bin/menu/addconditional?access_token=ACCESS_TOKEN
+	CreateConditionalMenu = "https://api.weixin.qq.com/cgi-bin/menu/addconditional"
+
+	//删除个性化菜单 POST
+	//https://api.weixin.qq.com/cgi-bin/menu/delconditional?access_token=ACCESS_TOKEN
+	DelConditionalMenu = "https://api.weixin.qq.com/cgi-bin/menu/delconditional"
+
+	//测试个性化菜单匹配结果 POST
+	//https://api.weixin.qq.com/cgi-bin/menu/trymatch?access_token=ACCESS_TOKEN
+	TryMatch = "https://api.weixin.qq.com/cgi-bin/menu/trymatch"
+
+	//获取自定义菜单配置 GET
+	//https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN
+	QueryConditionalMenu = "https://api.weixin.qq.com/cgi-bin/menu/get"
 )
