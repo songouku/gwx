3b7948a08c88a4141484944fc9c792d5fc728513
wbx <912005971@qq.com>
2019-11-13 17:43:57 +0800

    xml 解析测试用例

--- test/client_test.go
+++ test/client_test.go
@@ -1,9 +1,11 @@
 package test
 
 import (
+	"encoding/xml"
 	"fmt"
 	"gwx/client"
 	"gwx/util"
+	"io/ioutil"
 	"testing"
 )
 
@@ -19,4 +21,42 @@ func TestToken(t *testing.T) {
 		return
 	}
 	fmt.Printf("res is %s", util.ConvertStr(token))
+}
+
+type DataModel struct {
+	Persons Data
+}
+
+type Data struct {
+	Person []Person
+}
+
+type Person struct {
+	Name      string
+	Age       int
+	Career    string
+	Interests Interests
+}
+
+type Interests struct {
+	Interest []string
+}
+
+type Result struct {
+	Person []Person
+}
+
+func TestXmlParse(t *testing.T) {
+	str, err := ioutil.ReadFile("person.xml")
+	if err != nil {
+		t.Errorf("error is %v", err)
+		return
+	}
+	var result Result
+	err = xml.Unmarshal(str, &result)
+	if err != nil {
+		t.Errorf("error is %v", err)
+		return
+	}
+	fmt.Printf("result is %s", util.ConvertStr(result))
 }
